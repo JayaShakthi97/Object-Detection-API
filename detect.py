4726ece4670162597e7a570814003055cd7f2e1e
JayaShakthi97 <jayashakthiperera@gmail.com>
2021-11-24 08:55:46 +0530

    folder of images detection

--- detect.py
+++ detect.py
@@ -9,6 +9,8 @@ from yolov3_tf2.models import (
 )
 from yolov3_tf2.dataset import transform_images, load_tfrecord_dataset
 from yolov3_tf2.utils import draw_outputs
+import glob
+import csv
 
 flags.DEFINE_string('classes', './data/labels/coco.names', 'path to classes file')
 flags.DEFINE_string('weights', './weights/yolov3.tf',
@@ -17,8 +19,10 @@ flags.DEFINE_boolean('tiny', False, 'yolov3 or yolov3-tiny')
 flags.DEFINE_integer('size', 416, 'resize images to')
 flags.DEFINE_list('images', '/data/images/dog.jpg', 'list with paths to input images')
 flags.DEFINE_string('tfrecord', None, 'tfrecord instead of image')
+flags.DEFINE_string('testing_folder', None, 'folder path to input images')
 flags.DEFINE_string('output', './detections/', 'path to output folder')
 flags.DEFINE_integer('num_classes', 80, 'number of classes in the model')
+
 
 def main(_argv):
     physical_devices = tf.config.experimental.list_physical_devices('GPU')
@@ -36,7 +40,17 @@ def main(_argv):
     class_names = [c.strip() for c in open(FLAGS.classes).readlines()]
     print('classes loaded')
 
-    if FLAGS.tfrecord:
+    if FLAGS.testing_folder:
+        print('--------- Testing folder -------------')
+        testing_folder_path = FLAGS.testing_folder
+        images = glob.glob(testing_folder_path)
+        print(images)
+        raw_images = []
+        for image in images:
+            img_raw = tf.image.decode_image(
+                open(image, 'rb').read(), channels=3)
+            raw_images.append(img_raw)
+    elif FLAGS.tfrecord:
         dataset = load_tfrecord_dataset(
             FLAGS.tfrecord, FLAGS.classes, FLAGS.size)
         dataset = dataset.shuffle(512)
@@ -48,9 +62,10 @@ def main(_argv):
             img_raw = tf.image.decode_image(
                 open(image, 'rb').read(), channels=3)
             raw_images.append(img_raw)
-    num = 0    
+    num = 0
+    csv_content = []
     for raw_img in raw_images:
-        num+=1
+        num += 1
         img = tf.expand_dims(raw_img, 0)
         img = transform_images(img, FLAGS.size)
 
@@ -62,13 +77,24 @@ def main(_argv):
         print('detections:')
         for i in range(nums[0]):
             print('\t{}, {}, {}'.format(class_names[int(classes[0][i])],
-                                            np.array(scores[0][i]),
-                                            np.array(boxes[0][i])))
+                                        np.array(scores[0][i]),
+                                        np.array(boxes[0][i])))
 
         img = cv2.cvtColor(raw_img.numpy(), cv2.COLOR_RGB2BGR)
         img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
         cv2.imwrite(FLAGS.output + 'detection' + str(num) + '.jpg', img)
         print('output saved to: {}'.format(FLAGS.output + 'detection' + str(num) + '.jpg'))
+
+        detected_classes = []
+        for j in range(nums[0]):
+            detected_classes.append(class_names[int(classes[0][j])])
+        csv_row = [images[num - 1], 'detection' + str(num) + '.jpg']
+        csv_row.extend(detected_classes)
+        csv_content.append(csv_row)
+
+    with open('./detections/results.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(csv_content)
 
 
 if __name__ == '__main__':
