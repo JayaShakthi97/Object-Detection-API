70feba62cc54c69c424872e8014cfefd2cadc0bc
JayaShakthi97 <jayashakthiperera@gmail.com>
2021-11-24 18:24:44 +0530

    video detection fix

--- detect_video.py
+++ detect_video.py
@@ -9,14 +9,11 @@ from yolov3_tf2.models import (
 from yolov3_tf2.dataset import transform_images
 from yolov3_tf2.utils import draw_outputs
 
-
 flags.DEFINE_string('classes', './data/labels/coco.names', 'path to classes file')
-flags.DEFINE_string('weights', './weights/yolov3.tf',
-                    'path to weights file')
+flags.DEFINE_string('weights', './weights/yolov3.tf', 'path to weights file')
 flags.DEFINE_boolean('tiny', False, 'yolov3 or yolov3-tiny')
 flags.DEFINE_integer('size', 416, 'resize images to')
-flags.DEFINE_string('video', './data/video/paris.mp4',
-                    'path to video file or number for webcam)')
+flags.DEFINE_string('video', './data/video/paris.mp4', 'path to video file or number for webcam')
 flags.DEFINE_string('output', None, 'path to output video')
 flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
 flags.DEFINE_integer('num_classes', 80, 'number of classes in the model')
@@ -62,29 +59,29 @@ def main(_argv):
         if img is None:
             logging.warning("Empty Frame")
             time.sleep(0.1)
-            count+=1
+            count += 1
             if count < 3:
                 continue
-            else: 
+            else:
                 break
 
-        img_in = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+        img_in = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         img_in = tf.expand_dims(img_in, 0)
         img_in = transform_images(img_in, FLAGS.size)
 
         t1 = time.time()
         boxes, scores, classes, nums = yolo.predict(img_in)
-        fps  = ( fps + (1./(time.time()-t1)) ) / 2
+        fps = (fps + (1. / (time.time() - t1))) / 2
 
         img = draw_outputs(img, (boxes, scores, classes, nums), class_names)
         img = cv2.putText(img, "FPS: {:.2f}".format(fps), (0, 30),
                           cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 2)
-        
+
         if FLAGS.output:
             out.write(img)
-        cv2.imshow('output', img)
-        if cv2.waitKey(1) == ord('q'):
-            break
+        # cv2.imshow('output', img)
+        # if cv2.waitKey(1) == ord('q'):
+        #     break
 
     cv2.destroyAllWindows()
 
